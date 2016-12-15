619b842713a9bff1a111760e82c07bb900a25aa4
Thirumalaa Srinivas <thrmlsrnvs@gmail.com>
2016-12-14 21:43:43 -0600

    Adding functions, slices and array usage.

--- src/hello/main.go
+++ src/hello/main.go
@@ -4,6 +4,24 @@ import (
   "fmt"
 )
 
+func printer(aa []string) {
+  fmt.Printf("%d\n", len(aa))
+  for i, val := range aa {
+    fmt.Printf("%d %s\n", i, val)
+  }
+}
+
 func main() {
   fmt.Printf("Hello Worlds\n")
+
+  words := make([]string, 0, 5)
+  words = append(words, "srini")
+  words = append(words, "riddhi")
+  printer(words)
+
+  newWords := make([]string, 0, 5)
+  copy(newWords, words)
+  newWords = append(newWords, "rishi")
+  // newWords[1] = "rishi"
+  printer(newWords)
 }
