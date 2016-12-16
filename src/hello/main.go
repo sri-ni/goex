34eee5c54d0f69adacefdd9686bbcb662c9678de
Thirumalaa Srinivas <thrmlsrnvs@gmail.com>
2016-12-15 21:00:11 -0600

    Using maps, byte slicing and file reading.

--- src/hello/main.go
+++ src/hello/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-  "fmt"
+  "fmt";
+  "os"
 )
 
 func printer(aa []string) {
@@ -24,4 +25,38 @@ func main() {
   newWords = append(newWords, "rishi")
   // newWords[1] = "rishi"
   printer(newWords)
+
+  mapMaker()
+
+  byteSlicer()
+}
+
+func mapMaker() {
+  monthr := make(map[string]int)
+  monthr["jan"] = 31
+  monthr["feb"] = 28
+  monthr["mar"] = 31
+  for mo, da := range monthr {
+    fmt.Printf("%s has %d days\n", mo, da)
+  }
+
+  ftale := map[string]string {
+    "jack": "jill",
+    "fred": "wilma",
+    "hansel": "gretel",
+  }
+  for na, da := range ftale {
+    fmt.Printf("%s goes with %s\n", na, da)
+  }
+}
+
+func byteSlicer() {
+  b := make([]byte, 100)
+
+  f, err := os.Open("test.txt")
+  f.Read(b)
+  str := string(b[:])
+  if err != nil {
+    fmt.Printf("Read string = %d %s\n", len(b), str)
+  }
 }
